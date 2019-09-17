0cdb35945ca1198f9d830336989e3f9a82f81c79
Maximilian Schiller <manimax3@outlook.de>
2019-09-17 19:55:09 +0200

    Add math utility and glm dependency

---
+++ src/util.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <cstdint>
+#include <glm/glm.hpp>
+
+namespace eo {
+using int32  = std::int32_t;
+using uint   = unsigned int;
+using uint32 = std::uint32_t;
+using int8   = std::int8_t;
+using uint8  = std::uint8_t;
+using byte   = uint8;
+
+namespace math = glm;
+}
