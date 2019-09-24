e5acbb9b1289c2d2b99bb8d3568ba5dfd147347a
Maximilian Schiller <manimax3@outlook.de>
2019-09-24 20:52:16 +0200

    Implement a database migration mechanism

--- src/db.cpp
+++ src/db.cpp
@@ -4,11 +4,15 @@
 #include <sqlite3.h>
 #include <thread>
 
-std::shared_ptr<sqlite3> eo::db::make_database_connection(const std::string &file)
+std::shared_ptr<sqlite3> eo::db::make_database_connection(const std::string &file, bool migrate)
 {
     sqlite3 *db;
     if (sqlite3_open_v2(file.c_str(), &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE | SQLITE_OPEN_FULLMUTEX, nullptr) != SQLITE_OK) {
         throw std::runtime_error("Could not create/open database");
+    }
+
+    if (migrate) {
+        migrate_tables(*db, get_pragma_version(*db), CURRENT_VERSION);
     }
 
     return std::shared_ptr<sqlite3>{ db, [](auto *ptr) {
@@ -46,11 +50,33 @@ void eo::db::set_pragma_version(sqlite3 &dbconnection, int value)
     sqlite3_exec(&dbconnection, stmt.c_str(), nullptr, nullptr, nullptr);
 }
 
-void eo::db::create_tables(sqlite3 &dbconnection)
+void eo::db::migrate_tables(sqlite3 &dbconnection, int from, int to)
 {
-    const std::array statements = { "CREATE TABLE IF NOT EXISTS token(refreshtoken, charactername, characterid, accesstoken, expireson)" };
-
-    for (const auto &stmt : statements) {
-        sqlite3_exec(&dbconnection, stmt, nullptr, nullptr, nullptr);
+    if (from == to) {
+        return; // No work required
+    } else if (to - from > 1) {
+        migrate_tables(dbconnection, from, to - 1);
+        from = to - 1;
+    } else if (to < from) {
+        throw std::logic_error("migrate_tables: to is smaller than from value\n"
+                               "The database was probably created by a newer version");
     }
+
+    /*
+     * If db schema changes:
+     * 1. Bump version number in header
+     * 2. Write the conversion in the switch below
+     */
+
+    switch (from) {
+    case 0:
+        sqlite3_exec(&dbconnection, "CREATE TABLE IF NOT EXISTS token(refreshtoken, charactername, characterid, accesstoken, expireson)",
+                     nullptr, nullptr, nullptr);
+
+        break;
+    default:
+        throw std::logic_error(fmt::format("Unsupported database migration. from version {0} to version {1}", from, to));
+    }
+
+    set_pragma_version(dbconnection, to);
 }
--- src/db.h
+++ src/db.h
@@ -9,9 +9,13 @@ struct sqlite3_stmt;
 }
 
 namespace eo::db {
-std::shared_ptr<sqlite3>      make_database_connection(const std::string &file = get_exe_dir() + data_folder + "data.db");
+
+constexpr const int CURRENT_VERSION = 1;
+
+std::shared_ptr<sqlite3> make_database_connection(const std::string &file = get_exe_dir() + data_folder + "data.db", bool migrate = true);
 std::shared_ptr<sqlite3_stmt> make_statement(std::shared_ptr<sqlite3> dbconnection, const std::string &stmt);
-void                          create_tables(sqlite3 &dbconnection);
-int                           get_pragma_version(sqlite3 &dbconnection);
-void                          set_pragma_version(sqlite3 &dbconnection, int value);
+
+void migrate_tables(sqlite3 &dbconnection, int from, int to);
+int  get_pragma_version(sqlite3 &dbconnection);
+void set_pragma_version(sqlite3 &dbconnection, int value);
 }
